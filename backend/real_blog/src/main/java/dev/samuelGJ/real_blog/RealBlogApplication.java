b853c4a6f9ca0d0564f135031a1263b02928ab9a
samuelgbenga <samuelgbenga972@gmail.com>
2025-05-14 10:03:44 +0100

    init backend

---
+++ backend/real_blog/src/main/java/dev/samuelGJ/real_blog/RealBlogApplication.java
@@ -0,0 +1,13 @@
+package dev.samuelGJ.real_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealBlogApplication.class, args);
+	}
+
+}
---
+++ backend/real_blog/src/test/java/dev/samuelGJ/real_blog/RealBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.samuelGJ.real_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
